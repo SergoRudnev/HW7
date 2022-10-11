28a3c146f2d4c3e1ff41b47749bae03a97288104
Sergey Rudnev <sergorudnev@gmail.com>
2022-10-12 00:53:31 +0500

    Task47

---
+++ task47/Program.cs
@@ -0,0 +1,47 @@
+﻿int GetNumberFromConsole(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double GetRandomValue()
+{
+    return new Random().NextDouble();
+}
+
+void FillArray(double[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = GetRandomValue();
+        }
+    }
+}
+
+void PrintArray(double[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] InitializateArray(int m, int n)
+{
+    return new double[m,n];
+}
+
+//КОД ОСНОВНОЙ ПРОГРАММЫ
+int m = GetNumberFromConsole("Введите размерность массива m");
+int n = GetNumberFromConsole("Введите размерность массива n");
+double[,] array = InitializateArray(m,n);
+
+FillArray(array);
+
+PrintArray(array);
