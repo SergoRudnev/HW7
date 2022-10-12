ea8844d0c55bc7790139c33151a99a49e4ff57a0
Sergey Rudnev <sergorudnev@gmail.com>
2022-10-13 00:26:10 +0500

    Task 52

---
+++ 52/Program.cs
@@ -0,0 +1,56 @@
+﻿int GetNumberFromConsole(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetRandomValue()
+{
+    return new Random().Next(1,9);
+}
+
+void FillArray(int[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = GetRandomValue();
+        }
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] InitializateArray(int m, int n)
+{
+    return new int[m,n];
+}
+
+//КОД ОСНОВНОЙ ПРОГРАММЫ
+int m = GetNumberFromConsole("Введите размерность массива m");
+int n = GetNumberFromConsole("Введите размерность массива n");
+int[,] array = InitializateArray(m,n);
+FillArray(array);
+PrintArray(array);
+Console.WriteLine(array.GetLength(0));
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum += array[i, j];
+    }
+    Console.Write($"{ sum / array.GetLength(0)} ");
+}
+
